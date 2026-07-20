e77defab9c59603da74b9f043efb88ca302e2302
lovaszcsabamate-star <lovasz.csaba.mate@gmail.com>
2026-07-20 12:05:41 +0200

    Cache the final season-eligibility correction layer

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const PWA_CACHE = 'fociskartyak-2026-v7';
+const PWA_CACHE = 'fociskartyak-2026-v8';
 const PWA_SHELL = [
   './',
   './index.html',
@@ -28,6 +28,7 @@ const PWA_SHELL = [
   './data/club-official-enrichment-4-ujpest.json',
   './data/club-official-enrichment-5-other.json',
   './data/club-official-corrections.json',
+  './data/club-official-corrections-2.json',
   './data/club-official-sources.json',
   './assets/icons/icon.svg',
   './assets/qr/mobil-eleres.svg'
