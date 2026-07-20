abe751338f114e7d83059da96f241768534149df
lovaszcsabamate-star <lovasz.csaba.mate@gmail.com>
2026-07-20 21:57:55 +0200

    Harmadik Kisvárda-adatcsomag betöltése

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -14,6 +14,7 @@ const CLUB_ENRICHMENT_URLS = [
   'data/club-official-enrichment-6-eto-puskas.json',
   'data/club-official-enrichment-7-kisvarda-selected10.json',
   'data/club-official-enrichment-8-kisvarda-selected10.json',
+  'data/club-official-enrichment-9-kisvarda-selected10.json',
 ];
 const CLUB_CORRECTION_URLS = [
   'data/club-official-corrections.json',
@@ -25,6 +26,7 @@ const CLUB_STAT_PATCH_URLS = [
   'data/club-official-stat-patches-kisvarda.json',
   'data/club-official-stat-patches-kisvarda-selected10.json',
   'data/club-official-stat-patches-kisvarda-selected10-2.json',
+  'data/club-official-stat-patches-kisvarda-selected10-3.json',
 ];
 const CLUB_DIRECTORY_URL = 'data/club-official-sources.json';
 
