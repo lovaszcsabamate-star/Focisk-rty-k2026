a9785ede826ded4b4a22ac75e2ab50db49659be4
lovaszcsabamate-star <lovasz.csaba.mate@gmail.com>
2026-07-21 08:30:23 +0200

    Load MTK completion data in web app

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -18,6 +18,7 @@ const CLUB_ENRICHMENT_URLS = [
   'data/club-official-enrichment-10-kisvarda-final8.json',
   'data/club-official-enrichment-11-kisvarda-completion.json',
   'data/club-official-enrichment-12-dvtk-completion.json',
+  'data/club-official-enrichment-13-mtk-completion.json',
 ];
 const CLUB_CORRECTION_URLS = [
   'data/club-official-corrections.json',
@@ -33,6 +34,7 @@ const CLUB_STAT_PATCH_URLS = [
   'data/club-official-stat-patches-kisvarda-final8.json',
   'data/club-official-stat-patches-ferencvaros.json',
   'data/club-official-stat-patches-dvtk.json',
+  'data/club-official-stat-patches-mtk.json',
 ];
 const CLUB_DIRECTORY_URL = 'data/club-official-sources.json';
 
