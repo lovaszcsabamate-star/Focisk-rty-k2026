8386de693d84c13672f82b39893846643066dfc5
lovaszcsabamate-star <lovasz.csaba.mate@gmail.com>
2026-07-21 10:37:02 +0200

    Kazincbarcika 2025/26 teljes adatfeltöltése
    40/40 alapadat, 40/40 MLSZ Fizz Liga-statisztika, biztonságos többklubos kezelés, teljes web/PWA/Android integráció és tesztek.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const PWA_CACHE = 'fociskartyak-2026-v22';
+const PWA_CACHE = 'fociskartyak-2026-v23';
 const PWA_SHELL = [
   './',
   './index.html',
@@ -45,6 +45,7 @@ const PWA_SHELL = [
   './data/club-official-enrichment-13-mtk-completion.json',
   './data/club-official-enrichment-14-nyiregyhaza-completion.json',
   './data/club-official-enrichment-15-nyiregyhaza-nationalities.json',
+  './data/club-official-enrichment-16-kazincbarcika-completion.json',
   './data/club-official-corrections.json',
   './data/club-official-corrections-2.json',
   './data/club-official-corrections-3.json',
@@ -58,6 +59,7 @@ const PWA_SHELL = [
   './data/club-official-stat-patches-dvtk.json',
   './data/club-official-stat-patches-mtk.json',
   './data/club-official-stat-patches-nyiregyhaza.json',
+  './data/club-official-stat-patches-kazincbarcika.json',
   './data/club-official-sources.json',
   './assets/icons/icon.svg',
   './assets/qr/mobil-eleres.svg'
