50a64b1116c511155a0d4b18c2028f5f1909b4f8
lovaszcsabamate-star <lovasz.csaba.mate@gmail.com>
2026-07-21 11:47:29 +0200

    Paks kiegészítő adatfájljának bekötése

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -23,6 +23,7 @@ const CLUB_ENRICHMENT_URLS = [
   'data/club-official-enrichment-15-nyiregyhaza-nationalities.json',
   'data/club-official-enrichment-16-kazincbarcika-completion.json',
   'data/club-official-enrichment-17-ujpest-completion.json',
+  'data/club-official-enrichment-18-paks-completion.json',
 ];
 const CLUB_CORRECTION_URLS = [
   'data/club-official-corrections.json',
