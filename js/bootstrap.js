455274aa890d0248befb15170acde2bef77cbb1f
lovaszcsabamate-star <lovasz.csaba.mate@gmail.com>
2026-07-28 12:16:44 +0200

    Bootstrap szezonbetöltési aliasának visszafelé kompatibilis neve

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,5 +1,5 @@
 import {
-  loadActiveSeason as loadDatabaseSeason,
+  loadActiveSeason as loadDatabase,
 } from './database/season-service.js';
 import {
   buildQuickMatchPayload,
@@ -28,7 +28,7 @@ function showFatalError(error) {
 
 try {
   await installUiEnhancementPipeline();
-  const loaded = await loadDatabaseSeason();
+  const loaded = await loadDatabase();
   const {
     database,
     source,
