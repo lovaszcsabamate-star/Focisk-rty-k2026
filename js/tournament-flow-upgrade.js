3aa5ee69a7799153c92fd77e3289c11d667b8396
lovaszcsabamate-star <lovasz.csaba.mate@gmail.com>
2026-07-31 14:27:32 +0200

    Többlépcsős Torna mód belépési pont hozzáadása

---
+++ js/tournament-flow-upgrade.js
@@ -0,0 +1,5 @@
+/** Fociskártyák 2026 – Torna mód folyamat- és navigációfejlesztés. */
+
+import { installTournamentFlowUpgrade } from './tournament/tournament-flow-runtime.js';
+
+installTournamentFlowUpgrade();
