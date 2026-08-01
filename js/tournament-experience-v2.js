72446344c948c24d324c43a7a8931009d8ae4961
lovaszcsabamate-star <lovasz.csaba.mate@gmail.com>
2026-08-01 14:53:47 +0200

    Torna mód v2 indítómodul

---
+++ js/tournament-experience-v2.js
@@ -0,0 +1,4 @@
+/** Fociskártyák 2026 – Torna mód v2 élményréteg. */
+import { installTournamentExperienceV2 } from './tournament/tournament-experience-v2-runtime.js';
+installTournamentExperienceV2();
+export { installTournamentExperienceV2 };
